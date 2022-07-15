44f6a89911200960d3580b3f9102802c95474758
Paolo Federle <federlepaolo95@gmail.com>
2022-07-15 18:10:09 +0200

    first fizzbuzz attempt

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,45 @@
 // TODO: Write your function in this file
 
+function fizzBuzz(lower, upper) {
+    const fizzBuzzedArray = []
+    for (i=lower; i <= upper; i++) {
+        if (i%5 === 0 && i%3 ===0) {
+            fizzBuzzedArray.push("FizzBuzz")
+        }
+        else if (i%5 === 0) {
+            fizzBuzzedArray.push("Buzz")
+        }
+        else if (i%3 ===0) {
+            fizzBuzzedArray.push("Fizz")
+        }
+        else {
+            fizzBuzzedArray.push(i)
+        }
+    }
+    return fizzBuzzedArray
+}
+
+
+
+
+function fizzBuzz(lower, upper) {
+    const fizzBuzzedArray = []
+    for (i=lower; i <= upper; i++) {
+        let num = ""
+        if (i%3 === 0) {
+            num += "Fizz"
+        }
+        if (i%5 ===0) {
+            num += "Buzz"
+        }
+        if (num.length === 0) {
+            num = i
+        }
+        fizzBuzzedArray.push(num)
+    }
+    return fizzBuzzedArray
+}
+
+
 // TODO: Change undefined below to the name of your function
-module.exports = undefined
+module.exports = fizzBuzz
